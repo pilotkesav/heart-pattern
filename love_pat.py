7115439f46c6c100475cc7a843ef4eee7f2452b7
kesav kb <kesavabharathi2001@gmail.com>
2022-11-12 11:09:31 +0530

    heart

--- love_pat.py
+++ love_pat.py
@@ -7,9 +7,6 @@ st=(n//2)+1
 for i in range(st+1,n-1):
     m=i+i-n
     k=(i+i-m)//2
-    # print(m,k)
-    # print("k "*(n-i-1))
-    # print("* "*k," "*m,"* "*k,end="")
     mul("  ",n-1-i)
     s=i+i-1
     s1=n-i
@@ -17,7 +14,6 @@ for i in range(st+1,n-1):
     ac1=ac-3
     sr=s-ac1
     sr1=sr//2
-    # print("s",s,"s1",s1,"ac",ac1,"sr1",sr1)
     mul("* ",sr1)
     mul("  ",ac1)
     mul("* ",sr1)
